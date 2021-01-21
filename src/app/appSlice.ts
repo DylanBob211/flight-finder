cb2ba7beab51e52def61e8bf0453d746455aad85
Nicola D'Oronzo <nicola.doronzo@kingsrl.com>
2021-01-21 16:30:25 +0100

    add actions for flights and airlines

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -26,22 +26,34 @@ export const appSlice = createSlice({
     initialState,
     reducers: {
         getAirports: (state, action: PayloadAction<Airport[]>) => {
-            state.airports = action.payload
+            state.airports = action.payload;
         },
         setDeparture: (state, action: PayloadAction<string | null>) => {
-            state.departure = action.payload
+            state.departure = action.payload;
         },
         setArrival: (state, action: PayloadAction<string | null>) => {
-            state.arrival = action.payload
+            state.arrival = action.payload;
+        },
+        getBestFlight: (state, action: PayloadAction<Flight[]>) => {
+            state.flights = action.payload;
+        },
+        getAirlines: (state, action: PayloadAction<Airline[]>) => {
+            state.airlines = action.payload;
         }
     }
 });
 
 export const { setDeparture, setArrival } = appSlice.actions;
 
-export const getAirports = (): AppThunk => dispatch => {
-    api.getAirports().then(res => dispatch(appSlice.actions.getAirports(res.data.data)))
+export const getAirports = (): AppThunk => (dispatch) => {
+    api.getAirports().then((res) => dispatch(appSlice.actions.getAirports(res.data.data)));
+};
+export const getAirlines = (): AppThunk => dispatch => {
+    api.getAirlines().then(res => dispatch(appSlice.actions.getAirlines(res.data.data)))
 }
+export const searchBestFlight = (from: string, to: string): AppThunk => (dispatch) => {
+    api.getFlight(from, to).then((res) => dispatch(appSlice.actions.getBestFlight(res.data.data)));
+};
 
 export const selectAirports = (state: RootState) => state.app.airports;
 export const selectDeparture = (state: RootState) => state.app.departure;
