3507d4c3146e69f052a89698632ff92e6dac44b8
Nicola D'Oronzo <nicola.doronzo@kingsrl.com>
2021-01-22 04:00:19 +0100

    flights display improved

--- src/components/Flights.tsx
+++ src/components/Flights.tsx
@@ -1,9 +1,19 @@
-import { Paper, Typography } from '@material-ui/core';
+import { Box, makeStyles, Paper, Typography } from '@material-ui/core';
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { selectFlights, selectAirlines, selectAirports } from '../app/appSlice';
 
+const useStyles = makeStyles((theme) => ({
+    card: {
+        padding: theme.spacing(3),
+        margin: `${theme.spacing(3)}px 0`,
+        display: 'flex',
+        alignItems: 'center'
+    }
+}))
+
 const Flights = () => {
+    const classes = useStyles();
     const flights = useSelector(selectFlights);
     const airlines = useSelector(selectAirlines);
     const airports = useSelector(selectAirports);
@@ -20,6 +30,13 @@ const Flights = () => {
                             price={flight.price}
                         />),
                 )}
+            {
+                !!flights.length &&
+                <Paper elevation={5} className={classes.card} style={{ justifyContent: 'space-between' }}>
+                    <Typography variant="h6">Flights amount: {flights.length}</Typography>
+                    <Typography variant="h6">Total price: {flights.map(el => el.price).reduce((acc, cur) => acc + cur, 0).toFixed(2)}€</Typography>
+                </Paper>
+            }
         </>
     )
 }
@@ -32,11 +49,13 @@ interface FlightItemProps {
 }
 
 const FlightItem = ({ airline, departureAirport, arrivalAirport, price }: FlightItemProps) => {
-
+    const classes = useStyles();
     return (
-        <Paper style={{ padding: 20, margin: '20px 0', display: 'flex', justifyContent: 'space-around' }} >
-            <Typography variant="h6">{airline}</Typography>
-            <Typography variant="h6">{departureAirport}<span className="material-icons">flight</span>{arrivalAirport}</Typography>
+        <Paper className={classes.card} style={{ justifyContent: 'space-between' }} >
+            <Box>
+                <Typography variant="h6">{departureAirport}<span className="material-icons">flight</span>{arrivalAirport}</Typography>
+                <Typography variant="h6">{airline}</Typography>
+            </Box>
             <Typography variant="h6">{price}€</Typography>
         </Paper>
     )
